c64f8b8f79e8ce1658dcc50ef0196a03aaff45af
silvinavd <67972803+silvinavd@users.noreply.github.com>
2020-08-13 21:04:50 -0300

    Entrega 16/08

--- js/login.js
+++ js/login.js
@@ -2,5 +2,21 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("ingresar").addEventListener("click", function (e){
+let password = document.getElementById("inputPassword");
+let email = document.getElementById("inputEmail");
+let camposCompletos = true;
+
+
+if (password.value === '' || email.value === ''){
+            camposCompletos = false;
+            alert("Debes ingresar los datos!");
+        }
+
+
+if (camposCompletos){
+            window.location = 'index.html';
+        }
+  });
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,39 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var productsArray = [];
+
+function showProducts(array) { 
+
+    let contenido = "";
+    for (let i = 0; i < array.length; i++) {
+        let products = array[i];
+
+        contenido += 'Nombre: ' + products.name + '<br>';
+        contenido += 'Descripción: ' + products.description + '<br>';
+        contenido += 'Precio: ' + products.cost + '<br>';
+        contenido += '<br><hr><br>'
+        
+
+
+        document.getElementById("listado").innerHTML = contenido;
+    }
+} 
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+           productsArray = resultObj.data;
+
+            showProducts(productsArray);
+        }
+    });
+
+
+});
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
