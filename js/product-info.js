65e144124a98fd4bd74642fbb806bee4733e1741
silvinavd <67972803+silvinavd@users.noreply.github.com>
2020-09-19 10:32:42 -0300

    Entrega 4
    Productos relacionados
    Menú desplegable

--- js/product-info.js
+++ js/product-info.js
@@ -1,11 +1,29 @@
 var product = {};
-var arrayComments =[];
+var arrayComments = [];
+var productsArray = [];
 
-function showProduct(product,arrayComments) {
+
+function showRelatedProducts(arrayListado, arrayRelacionados) {
+    let contenido = "";
+    let product = arrayListado;
+
+    arrayRelacionados.forEach(function (i) {
+        contenido += "Nombre: " + product[i].name + "<br>";
+        contenido += "Precio: " + product[i].cost + "<br>";
+       // contenido += "Descripción: " + product[i].description + "<br>";
+       contenido += `
+        <img class="img-thumbnail" src= "${product[i].imgSrc}" width="60px" height = "40px">`;
+        contenido += `<a href="product-info.html"><button style ="float: right;" class= "btn btn-light active";>Ver más</button></a><br>`
+        contenido += `<br><hr><br>`
+    });
+    document.getElementById("relatedProduct").innerHTML = contenido;
+}
+
+function showProduct(product, arrayComments) {
     let imgs = "";
     let comments = "<hr>";
 
-    imgs +=`
+    imgs += `
     <img class="img" src= "${product.images[0]}" width="300px" height = "200px">
     <img class="img" src= "${product.images[1]}" width="300px" height = "200px">
     <img class="img" src= "${product.images[2]}" width="300px" height = "200px">
@@ -13,32 +31,32 @@ function showProduct(product,arrayComments) {
     <img class="img" src= "${product.images[4]}" width="300px" height = "200px">
     `;
 
-    arrayComments.forEach(function(comment){
+    arrayComments.forEach(function (comment) {
         let puntos = "";
         comments += `
         <strong>${comment.user} </strong> dice: <br>
         <p>${comment.description}</p>`;
 
-    for (let i=1; i<=comment.score; i++){
-        puntos += `<span class= "fa fa-star checked"></span>`;
-    }
-    for (let i= comment.score + 1; i<=5; i++){
-        puntos += `<span class= "fa fa-star"></span>`;
-    }
+        for (let i = 1; i <= comment.score; i++) {
+            puntos += `<span class= "fa fa-star checked"></span>`;
+        }
+        for (let i = comment.score + 1; i <= 5; i++) {
+            puntos += `<span class= "fa fa-star"></span>`;
+        }
 
-    comments += `<sub>${comment.dateTime}</sub><br>`;
-    comments += `<div style= "text-align: right;">${puntos}</div><br><hr>`;
+        comments += `<sub>${comment.dateTime}</sub><br>`;
+        comments += `<div style= "text-align: right;">${puntos}</div><br><hr>`;
 
 
 
-});
+    });
     document.getElementById("productImagesGallery").innerHTML = imgs;
-    document.getElementById ("comentarios").innerHTML = comments;
+    document.getElementById("comentarios").innerHTML = comments;
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_COMMENTS_URL).then (function(resultObj){
-        if (resultObj.status === "ok"){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             arrayComments = resultObj.data;
         }
     })
@@ -55,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
             let productDescriptionHTML = document.getElementById("categoryDescription");
             let productCountHTML = document.getElementById("productCount");
             let productsoldCount = document.getElementById("soldCount");
-            let productcategory = document.getElementById ("category");
+            let productcategory = document.getElementById("category");
 
             productNameHTML.innerHTML = product.name;
             productDescriptionHTML.innerHTML = product.description;
@@ -67,12 +85,21 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showProduct(product, arrayComments);
         }
     });
-});
+    
+   getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            productsArray = resultObj.data;
+
+            showRelatedProducts(productsArray, product.relatedProducts);
+
+        }
+    })
+})
 
 /*document.getElementById("enviarComentario").addEventListener("click", function(){
     let now= new Date();
     let dateTime = `${(now.getFullYear)()-${(now.getMonth()+1}}-${now.getDate()}`;
-    dateTime += `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`   
+    dateTime += `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
 
     let nuevo = {
         score: parseInt(document.getElementById("productCalification").value;
@@ -88,5 +115,5 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
     }
 
-    }     
+    }
 });*/
